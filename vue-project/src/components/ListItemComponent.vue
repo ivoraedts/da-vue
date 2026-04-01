ca95c27c72c969c924d05ebd045ca29cb1a0ae87
Ivo Raedts <ivoraedts@hotmail.com>
2026-04-01 18:50:07 +0200

    Played around a bit. Made some ugly list that can be displayed instead of the standard Welcome-List. (which looks better)

---
+++ vue-project/src/components/ListItemComponent.vue
@@ -0,0 +1,46 @@
+<template>  
+  <div class="item">
+    <i>
+      <slot name="icon"></slot>
+    </i>
+    <div class="details">
+      <slot name="linkingtext"></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.item {
+  margin-top: 2rem;
+  display: flex;
+  position: relative;
+}
+
+.details {
+  flex: 1;
+  margin-left: 1rem;
+}
+
+i {
+  display: flex;
+  place-items: center;
+  place-content: center;  
+}
+
+@media (min-width: 1024px) {
+  .item {
+    margin-top: 0;
+    padding: 0.4rem 0 1rem calc(var(--section-gap) / 2);
+  }
+
+  i {    
+    left: -2px;
+    position: absolute;
+    border: 1px solid var(--color-border);
+    background: var(--color-background);
+    border-radius: 8px;
+    width: 32px;
+    height: 30px;
+  }
+}
+</style>
---
+++ vue-project/src/components/TheList.vue
@@ -0,0 +1,99 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import type { ListItem } from '../models/listItem';
+import ListItemComponent from './ListItemComponent.vue';
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import VisualStudioCodeIcon from './icons/VisualStudioCodeIcon.vue'
+import YouTubeIcon from './icons/YouTubeIcon.vue'
+import VueIcon from './icons/VueIcon.vue';
+
+const itemList = ref<ListItem[]>([
+  {
+    title: 'Vite',
+    icon: 'documentation',
+    style: 'style-1',
+    ref: 'https://vite.dev/guide/features.html'
+  },
+  {
+    title: 'Using Vue in VSCode',
+    icon: 'vscode',
+    style: 'style-2',
+    ref: 'https://code.visualstudio.com/docs/nodejs/vuejs-tutorial'
+  },
+  {
+    title: 'Middle of Vue tutorial',
+    icon: 'icon-3',
+    style: 'style-3',
+    ref: 'https://vuejs.org/tutorial/#step-7'
+  }
+  ,
+  {
+    title: 'Some Youtube video about Vue with ASP.NET Core',
+    icon: 'youtube',
+    style: 'style-4',
+    ref: 'https://www.youtube.com/watch?v=HIIWdxEk_ls'
+  }
+])
+</script>
+
+<template>
+    <div v-for="listItem in itemList">        
+        <ListItemComponent>
+            <template #icon>
+                <div v-if="listItem.icon==='documentation'">
+                    <DocumentationIcon />
+                </div>
+                <div v-else-if="listItem.icon==='vscode'">
+                    <VisualStudioCodeIcon />
+                </div>
+                <div v-else-if="listItem.icon==='youtube'">
+                    <YouTubeIcon />
+                </div>
+                <div v-else>
+                    <VueIcon />
+                </div>
+            </template>
+            <template #linkingtext>
+                <div :class="listItem.style">
+                    <a :href="listItem.ref" target="_blank" rel="noopener noreferrer">
+                        {{ listItem.title }}
+                    </a>
+                </div>
+            </template>
+        </ListItemComponent>
+    </div>
+</template>
+
+<style scoped>
+.style-1 a {
+  color: greenyellow;
+}
+
+.style-1 a:hover{
+  color: darkgreen;
+}
+
+.style-2 a {
+  color: cadetblue;
+}
+
+.style-2 a:hover{
+  color: blue;
+}
+
+.style-3 a {
+  color: orange;
+}
+
+.style-3 a:hover{
+  color: orangered;
+}
+
+.style-4 a {
+  color: red;
+}
+
+.style-4 a:hover{
+  color: darkred;
+}
+</style>
