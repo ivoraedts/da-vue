ac214298f2c4a43247fc0002549c102ddde3d524
Ivo Raedts <ivoraedts@hotmail.com>
2026-04-02 18:08:21 +0200

    Moving the todo stuff away from App.vue into its own component

--- vuetify-project/src/App.vue
+++ vuetify-project/src/App.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
   import HelloWorld from '@/components/HelloWorld.vue'
   import Links from '@/components/Links.vue'
-  import { ref, type Ref, onMounted  } from 'vue'
+  import Todo from '@/components/Todo.vue'
+  import { ref, type Ref  } from 'vue'
 
   const showPage: Ref<string, string> = ref("home");
   showPage.value = "hello world page";
@@ -18,43 +19,9 @@
     showPage.value = "links page";
   }
 
-  // Define the structure to match your C# TodoItem model
-interface TodoItem {
-  id: number
-  task: string
-  isCompleted: boolean
-}
-
-const todoItems = ref<TodoItem[]>([])
-const loading = ref(true)
-const error = ref<string | null>(null)
-
-async function fetchTodos() {
-  loading.value = true
-  error.value = null
-  
-  try {
-    // Vite proxies '/api/todo' to 'http://localhost:5160/api/todo'
-    const response = await fetch('/api/todo')
-    
-    if (!response.ok) {
-      throw new Error(`API error: ${response.status}`)
-    }
-    
-    const data = await response.json()
-    todoItems.value = data
-  } catch (err) {
-    error.value = "Could not load tasks. Is the Backend running?"
-    console.error(err)
-  } finally {
-    loading.value = false
+  function showToDoPage() {
+    showPage.value = "todo page";
   }
-}
-
-onMounted(() => {
-  fetchTodos()
-})
-
 </script>
 
 <template>
@@ -71,6 +38,9 @@ onMounted(() => {
         <v-btn icon @click="showLinksPage()">
           <v-icon>mdi-list-box</v-icon>
         </v-btn>
+        <v-btn icon @click="showToDoPage()">
+          <v-icon>mdi-check-circle</v-icon>
+        </v-btn>
       </v-app-bar>
 
       <div v-if="showPage==='hello world page'" >
@@ -78,41 +48,16 @@ onMounted(() => {
       </div>
       <div v-else-if="showPage==='home'">
         <h1>Home Page</h1>
-        <v-container>
-        <v-card class="mx-auto mt-5" max-width="500">
-          <v-toolbar color="primary">
-            <v-toolbar-title>My .NET Tasks</v-toolbar-title>
-            <v-spacer></v-spacer>
-            <v-btn icon @click="fetchTodos" :loading="loading">
-              <v-icon>mdi-refresh</v-icon>
-            </v-btn>
-          </v-toolbar>
-
-          <v-list v-if="todoItems.length > 0">
-            <v-list-item v-for="item in todoItems" :key="item.id">
-              <template v-slot:prepend>
-                <v-checkbox-btn :model-value="item.isCompleted" readonly></v-checkbox-btn>
-              </template>
-              <v-list-item-title>{{ item.task }}</v-list-item-title>
-            </v-list-item>
-          </v-list>
-
-          <v-card-text v-else-if="!loading" class="text-center">
-            {{ error || 'No tasks found.' }}
-          </v-card-text>
-          
-          <v-progress-linear v-if="loading" indeterminate></v-progress-linear>
-        </v-card>
-      </v-container>
       </div>
       <div v-else-if="showPage==='links page'">
         <Links />
       </div>
+      <div v-else-if="showPage==='todo page'">
+        <Todo />
+      </div>
       <div v-else>
         <h1>Page Not Found</h1>
       </div>
-
-      
     </v-main>
     <v-btn
       class="ma-2"
