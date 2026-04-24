824043963be464820049ecc563799e9b5f139700
Ivo Raedts <ivoraedts@hotmail.com>
2026-04-24 11:31:05 +0200

    Added some colors to the v-cards on the overview page

--- vuetify-project/src/components/Home.vue
+++ vuetify-project/src/components/Home.vue
@@ -63,6 +63,7 @@ async function getLatestMeasurement() {
             showLatestMeasurement.value = true;
             latestMeasurement.value = data;
             console.log("Latest Measurement: " + data.insideTemperatureCelsius + "°C at " + data.retrievedAt);
+            setColorBasedOnTemperature(data.insideTemperatureCelsius);
         } else {
             showLatestMeasurement.value = false;
             console.log("No measurement data available");
@@ -81,6 +82,35 @@ const activeSchedule: Ref<TadoRetrievalScheduleModel | null> = ref(null);
 const showLatestMeasurement: Ref<boolean> = ref(false);
 const latestMeasurement: Ref<LatestMeasurement | null> = ref(null);
 
+const latestMeasurementColor: Ref<string> = ref("grey");
+function setColorBasedOnTemperature(temp: number) {
+    if (latestMeasurement.value) {
+
+        if (temp < 15) { latestMeasurementColor.value = "green-darken-4"; }
+        else if (temp < 16) { latestMeasurementColor.value = "green-darken-1"; }
+        else if (temp < 16.5) { latestMeasurementColor.value = "lime-accent-4"; }
+        else if (temp <= 17.0) { latestMeasurementColor.value = "lime-accent-3"; }
+        else if (temp <= 17.5) { latestMeasurementColor.value = "lime-accent-2"; }
+        else if (temp <= 18.5) { latestMeasurementColor.value = "yellow-lighten-4"; }
+        else if (temp <= 18.5) { latestMeasurementColor.value = "yellow-accent-4"; }
+        else if (temp <= 19.0) { latestMeasurementColor.value = "orange-lighten-4"; }
+        else if (temp <= 19.5) { latestMeasurementColor.value = "orange-lighten-2"; }
+        else if (temp <= 20.0) { latestMeasurementColor.value = "orange"; }
+        else if (temp <= 20.5) { latestMeasurementColor.value = "orange-darken-2"; }
+        else if (temp <= 21.0) { latestMeasurementColor.value = "orange-darken-3"; }
+        else if (temp <= 21.5) { latestMeasurementColor.value = "orange-darken-4"; }
+        else if (temp <= 22.0) { latestMeasurementColor.value = "red"; }
+        else if (temp <= 22.5) { latestMeasurementColor.value = "red-darken-2"; }
+        else if (temp <= 23.0) { latestMeasurementColor.value = "red-darken-3"; }
+        else if (temp <= 24.0) { latestMeasurementColor.value = "red-darken-4"; }
+        else if (temp <= 25.0) { latestMeasurementColor.value = "purple-darken-3"; }
+        else if (temp <= 26.0) { latestMeasurementColor.value = "purple-darken-4"; }
+        else if (temp <= 27.0) { latestMeasurementColor.value = "gray-darken-3"; }
+        else {
+            latestMeasurementColor.value = "gray-darken-4";
+        }
+    }
+}
 
 onMounted(() => {
     getActiveTrackingSchedule();
@@ -91,68 +121,70 @@ onMounted(() => {
 
 <template>
     <v-container v-if="showSection === 'overview'">
-        <v-card class="mx-auto mt-5" max-width="500">
+        <v-card variant="elevated" color="primary" class="mx-auto mt-5" max-width="500">
             <v-row>
                 <v-col cols="12" class="text-center">
                     <h1>Da Home Page</h1>
                 </v-col>
             </v-row>
-
-            <div v-if="canInitializeTracking">
-                <InitializeTracking />
-            </div>
-
-            <div v-if="showLatestMeasurement">
-                <v-row class="px-4 py-2" align="center">
-                    <v-col cols="12" class="text-center">
-                        <h2>Latest Measurement</h2>
-                    </v-col>
-                    <v-col cols="6">
-                        <p>Inside Temperature: {{ latestMeasurement?.insideTemperatureCelsius }} °C</p>
-                    </v-col>
-                    <v-col cols="6">
-                        <p>Humidity: {{ latestMeasurement?.humidityPercentage }} %</p>
-                    </v-col>
-                    <v-col cols="12" class="text-center">
-                        <p>Retrieved At: {{ new Date(latestMeasurement?.retrievedAt ?? "").toLocaleString() }}</p>
-                    </v-col>
-                </v-row>
-            </div>
-
-            <div v-if="canEditActiveSchedule">
-                <v-row class="px-4 py-2" align="center">
-                    <v-col cols="12" class="text-center">
-                        <v-row class="px-4 py-2" align="center">
-                            <v-col cols="6" class="text-right">
-                                <h2>Active Schedule</h2>
-                            </v-col>
-                            <v-col cols="6" class="text-left">
-                                <v-btn icon @click="showScheduleEditor()">
-                                    <v-icon>mdi-pencil</v-icon>
-                                </v-btn>
-                            </v-col>
-                        </v-row>
-                    </v-col>
-                    <v-col cols="6">
-                        <p>Schedule ID: {{ activeSchedule?.scheduleId }}</p>
-                        <p>Interval: {{ activeSchedule?.interval }} minutes</p>
-                        <p>Zone name: {{ activeSchedule?.zoneName }}</p>
-                        <p>Fail streak: {{ activeSchedule?.consecutiveFailures }}</p>
-                    </v-col>
-                    <v-col cols="6">
-                        <p>Next Retrieval:</p>
-                        <p>{{ activeSchedule?.nextRetrievalTimeString }}</p>
-                        <p>Last Retrieval:</p>
-                        <p>{{ activeSchedule?.lastRetrievalTimeString }}</p>
-                    </v-col>
-                    <v-col v-if="activeSchedule?.lastError" cols="12" class="text-center">
-                        <p>Last Error: {{ activeSchedule?.lastError }}</p>
-                    </v-col>
-                </v-row>
-            </div>
         </v-card>
+
+        <v-card v-if="canInitializeTracking" class="mx-auto mt-5" max-width="500">
+            <InitializeTracking />
+        </v-card>
+
+        <v-card variant="elevated" :color="latestMeasurementColor" v-if="showLatestMeasurement" class="mx-auto mt-5"
+            max-width="500">
+            <v-row class="px-4 py-2" align="center">
+                <v-col cols="12" class="text-center">
+                    <h2>Latest Measurement</h2>
+                </v-col>
+                <v-col cols="6">
+                    <p>Inside Temperature: {{ latestMeasurement?.insideTemperatureCelsius }} °C</p>
+                </v-col>
+                <v-col cols="6">
+                    <p>Humidity: {{ latestMeasurement?.humidityPercentage }} %</p>
+                </v-col>
+                <v-col cols="12" class="text-center">
+                    <p>Retrieved At: {{ new Date(latestMeasurement?.retrievedAt ?? "").toLocaleString() }}</p>
+                </v-col>
+            </v-row>
+        </v-card>
+
+        <v-card variant="elevated" color="secondary" v-if="canEditActiveSchedule" class="mx-auto mt-5" max-width="500">
+            <v-row class="px-4 py-2" align="center">
+                <v-col cols="12" class="text-center">
+                    <v-row class="px-4 py-2" align="center">
+                        <v-col cols="6" class="text-right">
+                            <h2>Active Schedule</h2>
+                        </v-col>
+                        <v-col cols="6" class="text-left">
+                            <v-btn icon @click="showScheduleEditor()">
+                                <v-icon>mdi-pencil</v-icon>
+                            </v-btn>
+                        </v-col>
+                    </v-row>
+                </v-col>
+                <v-col cols="6">
+                    <p>Schedule ID: {{ activeSchedule?.scheduleId }}</p>
+                    <p>Interval: {{ activeSchedule?.interval }} minutes</p>
+                    <p>Zone name: {{ activeSchedule?.zoneName }}</p>
+                    <p>Fail streak: {{ activeSchedule?.consecutiveFailures }}</p>
+                </v-col>
+                <v-col cols="6">
+                    <p>Next Retrieval:</p>
+                    <p>{{ activeSchedule?.nextRetrievalTimeString }}</p>
+                    <p>Last Retrieval:</p>
+                    <p>{{ activeSchedule?.lastRetrievalTimeString }}</p>
+                </v-col>
+                <v-col v-if="activeSchedule?.lastError" cols="12" class="text-center">
+                    <p>Last Error: {{ activeSchedule?.lastError }}</p>
+                </v-col>
+            </v-row>
+        </v-card>
+
     </v-container>
     <div v-if="showSection === 'schedule editor'">
-        <ScheduleEditor @response="(msg) => showOverviewWithComment(msg.action)"  />
+        <ScheduleEditor @response="(msg) => showOverviewWithComment(msg.action)" />
     </div>
 </template>
--- vuetify-project/src/components/InitializeTracking.vue
+++ vuetify-project/src/components/InitializeTracking.vue
@@ -11,38 +11,37 @@ const canStartStepper = computed(() => !stepperIsActive.value && !stepperIsDisab
 const canStopStepper = computed(() => stepperIsActive.value);
 
 function startStepper() {
-  stepperIsActive.value = true;
+    stepperIsActive.value = true;
 }
 
 function stopStepper() {
-  stepperIsActive.value = false;
+    stepperIsActive.value = false;
 }
 
 const currentStep = ref(1);
 const handleNext = () => {
-  if (currentStep.value === 3) {
-    addTrackingSchedule();
-  } else {
-    if (currentStep.value === 1) {
-        fetchUrl();
-    } else if (currentStep.value === 2) {
-        authenticateCommunication();
-    }
+    if (currentStep.value === 3) {
+        addTrackingSchedule();
+    } else {
+        if (currentStep.value === 1) {
+            fetchUrl();
+        } else if (currentStep.value === 2) {
+            authenticateCommunication();
+        }
 
-    currentStep.value++
-  }
+        currentStep.value++
+    }
 }
 
 const TadoInitializationData: Ref<TadoInitialization | null> = ref(null);
 const InitializationUrl = computed<string>(() => TadoInitializationData.value ? TadoInitializationData.value.verificationUriComplete : 'not available');
 
-async function fetchUrl()
-{
+async function fetchUrl() {
     // Vite proxies '/api/tadotemperature/get-new-url' to 'http://localhost:5160/api/tadotemperature/get-new-url'
     const response = await fetch('/api/tadotemperature/get-new-url')
-    
+
     if (!response.ok) {
-      throw new Error(`API error: ${response.status}`)
+        throw new Error(`API error: ${response.status}`)
     }
 
     const data = await response.json() as TadoInitialization;
@@ -50,13 +49,12 @@ async function fetchUrl()
 }
 
 const canStartStepper3 = computed(() => !stepperIsActive.value && !stepperIsDisabled.value);
-const ActualDataIsVisible = computed<boolean>(() => ActualTadoData.value !== null);    
+const ActualDataIsVisible = computed<boolean>(() => ActualTadoData.value !== null);
 const ActualTadoData: Ref<ActualTadoData | null> = ref(null);
-    
-async function authenticateCommunication()
-{
+
+async function authenticateCommunication() {
     // Vite proxies '/api/tadotemperature/authenticate' to 'http://localhost:5160/api/tadotemperature/authenticate'
-    const response = await fetch('/api/tadotemperature/authenticate/'+TadoInitializationData.value?.communicationId);
+    const response = await fetch('/api/tadotemperature/authenticate/' + TadoInitializationData.value?.communicationId);
 
     if (!response.ok) {
         throw new Error(`API error: ${response.status}`)
@@ -66,12 +64,12 @@ async function authenticateCommunication()
     ActualTadoData.value = data;
 }
 
-  const min = ref(1)
-  const max = ref(120)
-  const slider = ref(60)
+const min = ref(1)
+const max = ref(120)
+const slider = ref(60)
 
-  async function addTrackingSchedule() {
-    var newSchedule : SetSchedule = {
+async function addTrackingSchedule() {
+    var newSchedule: SetSchedule = {
         tokenId: ActualTadoData.value?.tokenId ?? 0,
         intervalInMinutes: slider.value
     }
@@ -79,125 +77,108 @@ async function authenticateCommunication()
     const response = await fetch('/api/tadotemperature/addSchedule', {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json'
+            'Content-Type': 'application/json'
         },
         body: JSON.stringify(newSchedule)
-      })
+    })
 
-      if (!response.ok) {
+    if (!response.ok) {
         throw new Error(`API error: ${response.status}`)
-      }
+    }
 
-      alert("Tracking schedule added successfully! The backend will now track your Tado temperatures at the specified interval.");
-      stopStepper();
-  }
+    alert("Tracking schedule added successfully! The backend will now track your Tado temperatures at the specified interval.");
+    stopStepper();
+}
 
 </script>
 
-<template>    
+<template>
 
 
-            <v-row v-if="canStartStepper" class="px-4 py-2" align="center">
-                <v-col cols="12" class="text-center">
-                    <v-btn color="primary" @click="startStepper">
-                        Initialize Tado Temperature Tracking
-                    </v-btn>
-                </v-col>
-            </v-row>
+    <v-row v-if="canStartStepper" class="px-4 py-2" align="center">
+        <v-col cols="12" class="text-center">
+            <v-btn color="primary" @click="startStepper">
+                Initialize Tado Temperature Tracking
+            </v-btn>
+        </v-col>
+    </v-row>
 
-            <v-row v-if="canStopStepper" class="px-4 py-2" align="center">
-                <v-col cols="12" class="text-center">
-                    <v-btn color="error" @click="stopStepper">
-                        Cancel Tado Temperature Tracking Initialisation
-                    </v-btn>
-                </v-col>
-            </v-row>
+    <v-row v-if="canStopStepper" class="px-4 py-2" align="center">
+        <v-col cols="12" class="text-center">
+            <v-btn color="error" @click="stopStepper">
+                Cancel Tado Temperature Tracking Initialisation
+            </v-btn>
+        </v-col>
+    </v-row>
 
-            <v-row v-if="stepperIsActive">
-                <v-col cols="12" class="text-center">
-                    <v-stepper
-                        alt-labels
-                        v-model="currentStep"
-                        prev-text="Previous"
-                        next-text="Next"
-                        :items="['Step 1', 'Step 2', 'Step 3']"
-                        hide-actions>
+    <v-row v-if="stepperIsActive">
+        <v-col cols="12" class="text-center">
+            <v-stepper alt-labels v-model="currentStep" prev-text="Previous" next-text="Next"
+                :items="['Step 1', 'Step 2', 'Step 3']" hide-actions>
 
-                    <template v-slot:item.1>
-                        <v-card title="Step One" flat>
-                            <v-row>
-                                <v-col cols="12" class="text-center">
-                                    <h2>Step One: Get Activation URL</h2>
-                                    <p>Click next to get the activation URL for your Tado account.</p>
-                                </v-col>
-                            </v-row>
-                        </v-card>
-                    </template>
+                <template v-slot:item.1>
+                    <v-card title="Step One" flat>
+                        <v-row>
+                            <v-col cols="12" class="text-center">
+                                <h2>Step One: Get Activation URL</h2>
+                                <p>Click next to get the activation URL for your Tado account.</p>
+                            </v-col>
+                        </v-row>
+                    </v-card>
+                </template>
 
-                    <template v-slot:item.2>
-                        <v-card title="Step Two" flat>
-                            <v-row>
-                                <v-col cols="12" class="text-center">
-                                    <h2>Step Two: Authorise Tado Account</h2>
-                                    <p>To authorise your Tado account, click the link below and follow the instructions and click Next when done.</p>
-                                    <a :href="InitializationUrl" target="_blank">{{ InitializationUrl }}</a>
-                                </v-col>
-                            </v-row>
-                        </v-card>
-                    </template>
+                <template v-slot:item.2>
+                    <v-card title="Step Two" flat>
+                        <v-row>
+                            <v-col cols="12" class="text-center">
+                                <h2>Step Two: Authorise Tado Account</h2>
+                                <p>To authorise your Tado account, click the link below and follow the instructions and
+                                    click Next when done.</p>
+                                <a :href="InitializationUrl" target="_blank">{{ InitializationUrl }}</a>
+                            </v-col>
+                        </v-row>
+                    </v-card>
+                </template>
 
-                    <template v-slot:item.3>
-                        <v-card title="Step Three" flat>
-                            <v-row>
-                                <v-col cols="12" class="text-center">
-                                    <h2>Step Three: Complete Initialisation</h2>
-                                    <div v-if="!ActualDataIsVisible">
-                                        <p>This should show some data to prove correctness and an interval to specify for tracking.</p>
-                                        <p>Click Finish to complete the initialisation process and start tracking your Tado temperatures.</p>
-                                    </div>
-                                    <div v-else>
-                                        <p>We successfully retrieved the Tado data. See here some actual values:</p>
-                                        <li>The Zone Name is {{ ActualTadoData?.zoneName }}</li>
-                                        <li>The temperature is {{ ActualTadoData?.insideTemperatureCelsius }} degrees Celsius.</li>
-                                        <li>The Humidity is {{ ActualTadoData?.humidityPercentage }} %</li>
-                                        <li>The (Tado) Home ID is {{ ActualTadoData?.homeId }}</li>
-                                        <li>The Token is stored in our database and the ID is {{ ActualTadoData?.tokenId }}</li>
-                                        <p>Set the tracking interval and click Finish to complete the initialisation process and start tracking your Tado temperatures.</p>
-                                        <v-slider
-                                            v-model="slider"
-                                            :max="max"
-                                            :min="min"
-                                            :step="1"
-                                            class="align-center"
-                                            hide-details
-                                            color="green"
-                                            thumb-color="orange"
-                                        >
-                                            <template v-slot:append>
-                                            <v-text-field
-                                                v-model="slider"
-                                                density="compact"
-                                                style="width: 70px"
-                                                type="number"
-                                                hide-details
-                                                single-line
-                                            ></v-text-field>
-                                            </template>
-                                        </v-slider>
-                                    </div>
-                                </v-col>
-                            </v-row>
-                        </v-card>
-                    </template>
+                <template v-slot:item.3>
+                    <v-card title="Step Three" flat>
+                        <v-row>
+                            <v-col cols="12" class="text-center">
+                                <h2>Step Three: Complete Initialisation</h2>
+                                <div v-if="!ActualDataIsVisible">
+                                    <p>This should show some data to prove correctness and an interval to specify for
+                                        tracking.</p>
+                                    <p>Click Finish to complete the initialisation process and start tracking your Tado
+                                        temperatures.</p>
+                                </div>
+                                <div v-else>
+                                    <p>We successfully retrieved the Tado data. See here some actual values:</p>
+                                    <li>The Zone Name is {{ ActualTadoData?.zoneName }}</li>
+                                    <li>The temperature is {{ ActualTadoData?.insideTemperatureCelsius }} degrees
+                                        Celsius.</li>
+                                    <li>The Humidity is {{ ActualTadoData?.humidityPercentage }} %</li>
+                                    <li>The (Tado) Home ID is {{ ActualTadoData?.homeId }}</li>
+                                    <li>The Token is stored in our database and the ID is {{ ActualTadoData?.tokenId }}
+                                    </li>
+                                    <p>Set the tracking interval and click Finish to complete the initialisation process
+                                        and start tracking your Tado temperatures.</p>
+                                    <v-slider v-model="slider" :max="max" :min="min" :step="1" class="align-center"
+                                        hide-details color="green" thumb-color="orange">
+                                        <template v-slot:append>
+                                            <v-text-field v-model="slider" density="compact" style="width: 70px"
+                                                type="number" hide-details single-line></v-text-field>
+                                        </template>
+                                    </v-slider>
+                                </div>
+                            </v-col>
+                        </v-row>
+                    </v-card>
+                </template>
 
-                    <v-stepper-actions
-                        :disabled="false"
-                        :next-text="currentStep === 3 ? 'Finish' : 'Next'"
-                        @click:next="handleNext"
-                        @click:prev="currentStep--"
-                    ></v-stepper-actions>
-                    </v-stepper>
-                </v-col>
-            </v-row>
+                <v-stepper-actions :disabled="false" :next-text="currentStep === 3 ? 'Finish' : 'Next'"
+                    @click:next="handleNext" @click:prev="currentStep--"></v-stepper-actions>
+            </v-stepper>
+        </v-col>
+    </v-row>
 
 </template>
