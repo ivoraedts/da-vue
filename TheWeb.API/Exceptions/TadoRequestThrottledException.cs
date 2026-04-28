74d9a5f3592dbf490da9ed7e199349aa036ef857
Ivo Raedts <ivoraedts@hotmail.com>
2026-04-29 00:05:55 +0200

    bugfix... constructor parameter overwrites itself instead of setting the property it should set. stupid AI.

--- TheWeb.API/Exceptions/TadoRequestThrottledException.cs
+++ TheWeb.API/Exceptions/TadoRequestThrottledException.cs
@@ -4,8 +4,8 @@ public class TadoRequestThrottledException : Exception
 {
     public int NumberOfConsecutiveFailures { get; set; }
 
-    public TadoRequestThrottledException(int NumberOfConsecutiveFailures, string message) : base(message)
+    public TadoRequestThrottledException(int numberOfConsecutiveFailures, string message) : base(message)
     {
-        NumberOfConsecutiveFailures = NumberOfConsecutiveFailures;
+        NumberOfConsecutiveFailures = numberOfConsecutiveFailures;
     }
 }
