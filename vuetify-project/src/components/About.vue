8d1fcf203aa968a6c4296d17212010d210df1813
Ivo Raedts <ivoraedts@hotmail.com>
2026-04-30 17:44:55 +0200

    minor graphical change and auto-allign

--- vuetify-project/src/components/About.vue
+++ vuetify-project/src/components/About.vue
@@ -1,11 +1,7 @@
 <template>
   <v-container class="fill-height d-flex flex-column justify-center" max-width="1100">
     <div>
-      <v-img
-        class="mb-4 font-weight-bold"
-        height="150"
-        src="@/assets/logo-medium.png"
-      />
+      <v-img class="mb-4 font-weight-bold" height="150" src="@/assets/logo-medium.png" />
 
       <div class="mb-8 text-center">
         <div class="text-body-medium font-weight-light mb-n1">The app was built just to play around with</div>
@@ -14,13 +10,7 @@
 
       <v-row>
         <v-col cols="12">
-          <v-card
-            class="py-4"
-            color="surface-variant"
-            
-            rounded="lg"
-            variant="tonal"
-          >
+          <v-card class="py-4" color="surface-variant" rounded="lg" variant="tonal">
             <template #prepend>
               <v-avatar class="ml-2 mr-4" icon="mdi-rocket-launch-outline" size="60" variant="tonal" />
             </template>
@@ -30,53 +20,36 @@
             </template>
 
             <template #title>
-              <div class="my-title my-uppercase text-headline-medium font-weight-bold">Got started with Vue</div>
+              <div class="my-title my-uppercase text-headline-medium font-weight-bold">Got started with
+                <br class="hidden-md-and-up" />
+                Vue
+              </div>
             </template>
 
             <template #subtitle>
               <div class="text-body-large">
                 <p class="text-wrap">
-                  I got started to play around with Vue and then figured out that Vuetify is a nice way to quickly build a nice looking UI in Material Design.
+                  I got started to play around with Vue and then figured out that Vuetify is a nice way to quickly build
+                  a nice looking UI in Material Design.
                   <br />
                   The source code of this project is available on GitHub and even on Docker Hub:
                 </p>
                 <v-row>
-                <v-col cols="4">
-                  <v-card 
-                    prepend-icon="mdi-github"
-                    title="Da Vue on Github"
-                    subtitle="https://github.com/ivoraedts/da-vue"
-                    href="https://github.com/ivoraedts/da-vue"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    color="surface-variant"
-                    rounded="lg"
-                  ></v-card>
-                </v-col>
-                <v-col cols="3">
-                  <v-card 
-                    prepend-icon="mdi-docker"
-                    title="Docker Hub"
-                    subtitle="Da Vue Web.API"
-                    href="https://hub.docker.com/repository/docker/ivoraedts/da-vue-theweb-api/general"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    color="surface-variant"
-                    rounded="lg"
-                  ></v-card>
-                </v-col>
-                <v-col cols="3">
-                  <v-card 
-                    prepend-icon="mdi-docker"
-                    title="Docker Hub"
-                    subtitle="Da Vue Web.UI"
-                    href="https://hub.docker.com/repository/docker/ivoraedts/da-vue-theweb-ui/general"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    color="surface-variant"
-                    rounded="lg"
-                  ></v-card>
-                </v-col>                
+                  <v-col cols="4">
+                    <v-card prepend-icon="mdi-github" title="Da Vue on Github"
+                      subtitle="https://github.com/ivoraedts/da-vue" href="https://github.com/ivoraedts/da-vue"
+                      target="_blank" rel="noopener noreferrer" color="surface-variant" rounded="lg"></v-card>
+                  </v-col>
+                  <v-col cols="3">
+                    <v-card prepend-icon="mdi-docker" title="Docker Hub" subtitle="Da Vue Web.API"
+                      href="https://hub.docker.com/repository/docker/ivoraedts/da-vue-theweb-api/general"
+                      target="_blank" rel="noopener noreferrer" color="surface-variant" rounded="lg"></v-card>
+                  </v-col>
+                  <v-col cols="3">
+                    <v-card prepend-icon="mdi-docker" title="Docker Hub" subtitle="Da Vue Web.UI"
+                      href="https://hub.docker.com/repository/docker/ivoraedts/da-vue-theweb-ui/general" target="_blank"
+                      rel="noopener noreferrer" color="surface-variant" rounded="lg"></v-card>
+                  </v-col>
                 </v-row>
               </div>
             </template>
