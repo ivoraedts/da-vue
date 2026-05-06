3626e0f74320465a51e2f423a2fa49e0658150f0
Ivo Raedts <ivoraedts@hotmail.com>
2026-05-06 11:50:18 +0200

    Improved the look on the phone for the menu

--- vuetify-project/src/App.vue
+++ vuetify-project/src/App.vue
@@ -1,27 +1,27 @@
 <script setup lang="ts">
-  import About from '@/components/About.vue'
-  import Links from '@/components/Links.vue'
-  import Todo from '@/components/Todo.vue'
-  import Home from '@/components/Home.vue'
-  import { ref, type Ref  } from 'vue'
+import About from '@/components/About.vue'
+import Links from '@/components/Links.vue'
+import Todo from '@/components/Todo.vue'
+import Home from '@/components/Home.vue'
+import { ref, type Ref } from 'vue'
 
-  const showPage: Ref<string, string> = ref("home");
+const showPage: Ref<string, string> = ref("home");
 
-  function showAboutPage() {
-    showPage.value = "about page";
-  }
+function showAboutPage() {
+  showPage.value = "about page";
+}
 
-  function showHomePage() {
-    showPage.value = "home";
-  }
+function showHomePage() {
+  showPage.value = "home";
+}
 
-  function showLinksPage() {
-    showPage.value = "links page";
-  }
+function showLinksPage() {
+  showPage.value = "links page";
+}
 
-  function showToDoPage() {
-    showPage.value = "todo page";
-  }
+function showToDoPage() {
+  showPage.value = "todo page";
+}
 </script>
 
 <template>
@@ -31,40 +31,46 @@
         <template v-slot:prepend>
           <v-app-bar-nav-icon @click="showHomePage()"></v-app-bar-nav-icon>
         </template>
-        <v-app-bar-title>Da Vue</v-app-bar-title>
+        <v-app-bar-title :class="$vuetify.display.xs ? 'phone-menu-title' : ''">
+          <span>Da Vue</span>
+        </v-app-bar-title>
         <v-btn append-icon="mdi-information-outline" color="green-darken-4" @click="showAboutPage()">
-          About
+          <span :class="$vuetify.display.xs ? 'phone-menu-txt' : ''">About</span>
         </v-btn>
         <v-btn append-icon="mdi-list-box" color="blue-darken-2" @click="showLinksPage()">
-          Links
+          <span :class="$vuetify.display.xs ? 'phone-menu-txt' : ''">Links</span>
         </v-btn>
         <v-btn append-icon="mdi-check-circle" color="red-darken-2" @click="showToDoPage()">
-          Todos
+          <span :class="$vuetify.display.xs ? 'phone-menu-txt' : ''">Todos</span>
         </v-btn>
       </v-app-bar>
 
-      <div v-if="showPage==='about page'" >
-        <About />  
+      <div v-if="showPage === 'about page'">
+        <About />
       </div>
-      <div v-else-if="showPage==='home'">
+      <div v-else-if="showPage === 'home'">
         <Home />
       </div>
-      <div v-else-if="showPage==='links page'">
+      <div v-else-if="showPage === 'links page'">
         <Links />
       </div>
-      <div v-else-if="showPage==='todo page'">
+      <div v-else-if="showPage === 'todo page'">
         <Todo />
       </div>
       <div v-else>
         <h1>Page Not Found</h1>
       </div>
     </v-main>
-    <v-btn
-      class="ma-2"
-      icon="mdi-theme-light-dark"
-      location="top right"
-      position="absolute"
-      @click="$vuetify.theme.cycle()"
-    />
+    <v-btn class="ma-2" icon="mdi-theme-light-dark" location="top right" position="absolute"
+      @click="$vuetify.theme.cycle()" />
   </v-app>
 </template>
+<style scoped>
+.phone-menu-title{
+  margin-left: -0px;
+}
+.phone-menu-txt {
+  margin-left: -7px;
+  margin-right: -4px;
+}
+</style>
